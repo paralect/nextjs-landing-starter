89968eac96509df99e258a9e4d804f36aacc5297
Evgeny Zhivitsa <evgeny.zhivitsa@gmail.com>
2018-09-10 22:52:57 +0300

    [*] Fix production build

--- src/client/pages/_document.jsx
+++ src/client/pages/_document.jsx
@@ -11,8 +11,8 @@ export default class CustomDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
           <link rel="shortcut icon" href="static/favicon.ico" />
           <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet" />
-          <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
+
         <body>
           <Main />
           <NextScript />
